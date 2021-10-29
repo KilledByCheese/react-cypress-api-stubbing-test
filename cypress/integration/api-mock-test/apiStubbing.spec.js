ef9d66798b384a46e9315ed88809c4f04ff71715
boening <Marcel.Boening@adesso.de>
2021-10-29 15:50:24 +0200

    added comments

--- cypress/integration/api-mock-test/apiStubbing.spec.js
+++ cypress/integration/api-mock-test/apiStubbing.spec.js
@@ -1,20 +1,20 @@
 
 describe("Cypress Mock API Tests", () => {
-    beforeEach(() => {
-        cy.visit("http://localhost:3000")
+    beforeEach(() => {                      //Before each Test
+        cy.visit("http://localhost:3000")   //Load the application
     });
 
-    it("Liste Leer", () => {
-        cy.get("#UserList")
-            .should("not.be.visible");     
+    it("User List not visible at the beginning", () => {
+        cy.get("#UserList")             //get the User List by ID
+            .should("not.be.visible");  //Should not be visible because it's empty
     });
 
     it("Mock API call on Button Click", () => {
-        cy.intercept(
+        cy.intercept(                               //Stub/Mock/Intercept API calls
             {
-                method: "get", //Route all get Requests
-                url: "https://randomuser.me/api/*" //that have a URL that matches
-            }, //return the following
+                method: "get",                      //Route all get Requests
+                url: "https://randomuser.me/api/*"  //that have a URL that matches
+            },                                      //return the following data
             {results:[
                 {
                     name:{
@@ -37,14 +37,21 @@ describe("Cypress Mock API Tests", () => {
                     }
                 }
             ]}
-        ).as("getUsers"); //and assign an alias
+        ).as("getUsers");                   //and assign an alias
 
-        cy.get("#UserButton")
-            .click()
-            .get("#UserList").children()
-            .should("have.length", 2);
+        cy.get("#UserButton")               //Get the Button by ID
+            .click()                        //Click it
+            .get("#UserList").children()    //Get the User List by ID
+            .should("have.length", 2);      //Check the Length
 
-        cy.get("#UserList").children().first().should("have.text","Mr Max Mustermann");
-        cy.get("#UserList").children().eq(1).should("have.text","Mrs Lisa Musterfrau");
+        cy.get("#UserList")                             //Get the User List by ID
+            .children()                                 //get the child elements
+            .first()                                    //get the first child element
+            .should("have.text","Mr Max Mustermann");   //Check the Content
+
+        cy.get("#UserList")                             //Get the User List by ID
+            .children()                                 //get the child elements
+            .eq(1)                                      //get the second child Element
+            .should("have.text","Mrs Lisa Musterfrau"); //check the Content
     });
   });
--- src/App.js
+++ src/App.js
@@ -1,20 +1,19 @@
-import logo from './logo.svg';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { Button } from 'react-bootstrap';
 import axios from 'axios';
 import React, { useState } from 'react';
 
-function App() {
+export default function App() {
 
-  const [users, setUsers] = useState([]);
+  const [users, setUsers] = useState([]); //Array to manage Users in state - default empty Array
 
   function makeAPICall() {
-    axios.get("https://randomuser.me/api/?results=2")
-      .then(res => {
-        console.log(res.data);
-        setUsers(res.data.results)
-      })
+    axios.get("https://randomuser.me/api/?results=2") //Request Data from randomuser API with Axios
+      .then(res => {                                  
+        console.log(res.data);                        //log the result
+        setUsers(res.data.results)                    //update the state
+      });
   }
 
   return (
@@ -22,10 +21,9 @@ function App() {
       <Button variant="primary" id="UserButton" onClick={makeAPICall}>Generiere Neue User</Button>
       <h1>Beispiel-User Liste</h1>
       <ul id="UserList">
+        {/* Mapping each entry of the users Array to a <li> element and setting the user names as content*/}
         {users.map(user => <li key={user.login.uuid} >{user.name.title}{" "}{user.name.first}{" "}{user.name.last}</li> )}
       </ul>
     </div>
   );
 }
-
-export default App;
